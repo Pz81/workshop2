d8ce4a69268f50e9d18af4dca54d3059c8b16e30
Pz81 <113420015+Pz81@users.noreply.github.com>
2022-12-20 10:34:11 +0100

    Module4_workshop2

---
+++ script.js
@@ -0,0 +1,53 @@
+document.getElementById("c").onclick = function(){ // als op de c-toets wordt geklikt
+	var c_toets = document.getElementById("audio_1"); // zoek het c-toets audio-element op
+	c_toets.play(); // speel het audio-element af
+}
+
+document.getElementById("d").onclick = function(){
+	var d_toets = document.getElementById("audio_2");
+	d_toets.play();
+}
+
+document.getElementById("e").onclick = function(){
+	console.log(document.getElementById("audio_e"));
+	var e_toets = document.getElementById("audio_e");
+	e_toets.play();
+    //alert("test");
+}
+document.getElementById("f").onclick = function(){
+	var e_toets = document.getElementById("audio_f");
+	e_toets.play();
+    //alert("test");
+}
+document.getElementById("g").onclick = function(){
+	var e_toets = document.getElementById("audio_g");
+	e_toets.play();
+    //alert("test");
+}
+document.getElementById("h").onclick = function(){
+	var e_toets = document.getElementById("audio_h");
+	e_toets.play();
+    //alert("test");
+}
+document.getElementById("i").onclick = function(){
+	var e_toets = document.getElementById("audio_i");
+	e_toets.play();
+    //alert("test");
+}
+document.getElementById("j").onclick = function(){
+	var e_toets = document.getElementById("audio_j");
+	e_toets.play();
+    //alert("test");
+}
+document.getElementById("k").onclick = function(){
+	var e_toets = document.getElementById("audio_k");
+	e_toets.play();
+   // alert("test");
+}
+document.getElementById("l").onclick = function(){
+	var e_toets = document.getElementById("audio_l");
+	e_toets.play();
+   // alert("test");
+}
+
+console.log(document.getElementById("e"));
